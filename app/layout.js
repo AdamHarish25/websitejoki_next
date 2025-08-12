f0c3098ccde23dba2965982119f487fae13de93b
AdamHarish25 <adamharits25@gmail.com>
2025-08-12 14:34:44 +0700

    build 10.1

--- app/layout.js
+++ app/layout.js
@@ -13,7 +13,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {  
   return (
-    <html lang="en">
+    <html lang="id">
       {/* 2. Tambahkan dua komponen Script ini di dalam <html>, di atas <body> */}
       <Script
         strategy="afterInteractive"
